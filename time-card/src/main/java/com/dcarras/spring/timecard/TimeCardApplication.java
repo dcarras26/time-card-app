cf2fdf6f674e27521e36c05b4fccd8e145f74919
Dennis Carrasquillo <denniscarrasquillo@Lindsays-MacBook-Air.local>
2022-02-14 13:18:57 -0500

    Initialized project

---
+++ time-card/src/main/java/com/dcarras/spring/timecard/TimeCardApplication.java
@@ -0,0 +1,13 @@
+package com.dcarras.spring.timecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCardApplication.class, args);
+	}
+
+}
---
+++ time-card/src/test/java/com/dcarras/spring/timecard/TimeCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcarras.spring.timecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
